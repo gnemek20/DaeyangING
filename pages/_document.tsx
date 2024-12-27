69f5caa73363887de32473586e5229023b467758
gnemek20 <gnemek20@gmail.com>
2024-12-28 08:25:40 +0900

    init

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <meta name="description" content="패션의 완성 대양ING" />
+      </Head>
       <body>
         <Main />
         <NextScript />
