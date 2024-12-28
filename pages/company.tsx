4fdf75b4363224bab3b7b4dacf146dcee1581cf5
gnemek20 <gnemek20@gmail.com>
2024-12-28 09:58:33 +0900

    랜딩, 회사 소개 페이지 윤곽 추가

---
+++ pages/company.tsx
@@ -0,0 +1,54 @@
+import styles from "@/styles/Company.module.css";
+
+const Company = () => {
+  return (
+    <>
+      <div className={`${styles.introduce}`}>
+        <div>
+          <div className={`${styles.coverImage}`}>
+            {/* 이미지 */}
+          </div>
+        </div>
+        <div>
+          <div className={`${styles.introduceText}`}>
+            <h1>회사 소개하는 글</h1>
+            <div>
+              <p>장문의 글을 사용해주세요.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={`${styles.merit}`}>
+        <div className={`${styles.inspiringText}`}>
+          <hr />
+          <div>
+            <h1>장점을 소개하는 문구</h1>
+          </div>
+          <hr />
+        </div>
+        <div className={`${styles.advantages}`}>
+          <div>
+            <div className={`${styles.advantageTitle}`}>
+              <h1>장점</h1>
+              <hr />
+            </div>
+          </div>
+          <div>
+            <div className={`${styles.advantageTitle}`}>
+              <h1>장점</h1>
+              <hr />
+            </div>
+          </div>
+          <div>
+            <div className={`${styles.advantageTitle}`}>
+              <h1>장점</h1>
+              <hr />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Company;
---
+++ pages/index.tsx
@@ -0,0 +1,33 @@
+import styles from "@/styles/Landing.module.css";
+
+const Landing = () => {
+  return (
+    <>
+      <div>
+        {/* 랜딩 애니메이션 */}
+      </div>
+      <div className={`${styles.summary}`}>
+        <div className={`${styles.inspiringText}`}>
+          <hr />
+          <div>
+            <h1>패션의 완성을 위해 달리는 당신에게</h1>
+            <h1>날개를 달아드리겠습니다.</h1>
+          </div>
+          <hr />
+        </div>
+        <div className={`${styles.inspiringSights}`}>
+          <div className={`${styles.comeDownImages}`}>
+            {/* 이미지들 추가 */}
+          </div>
+          <hr />
+          <div className={`${styles.actionButtons}`}>
+            <button>More View</button>
+            <button>Contact</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Landing;
